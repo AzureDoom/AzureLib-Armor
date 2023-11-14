ffff151d8ff8405171421ddec6d40b4a0395bb96
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-11-14 10:45:44 -0500

    Remove old

--- Fabric/src/main/java/mod/azure/azurelibarmor/core/object/PlayState.java
+++
@@ -1,9 +0,0 @@
-package mod.azure.azurelibarmor.core.object;
-
-/**
- * State enum to define whether an {@link mod.azure.azurelibarmor.core.animation.AnimationController} should continue or stop
- */
-public enum PlayState {
-	CONTINUE,
-	STOP
-}
