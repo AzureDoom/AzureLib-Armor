617954d94544b61fc2b0666a048f7c26090e13ca
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-06-25 20:30:08 -0400

    Add a helper SingleteonGeoAnimatable#registerSyncedAnimatable

--- common/src/main/java/mod/azure/azurelibarmor/common/api/common/animatable/GeoItem.java
+++ common/src/main/java/mod/azure/azurelibarmor/common/api/common/animatable/GeoItem.java
@@ -48,6 +48,15 @@ public interface GeoItem extends SingletonGeoAnimatable {
     }
 
     /**
+     * Register this as a synched {@code GeoAnimatable} instance with AzureLib's networking functions
+     * <p>
+     * This should be called inside the constructor of your object.
+     */
+    static void registerSyncedAnimatable(GeoAnimatable animatable) {
+        SingletonGeoAnimatable.registerSyncedAnimatable(animatable);
+    }
+
+    /**
      * Gets the unique identifying number from this ItemStack's {@link net.minecraft.nbt.Tag NBT}, or
      * {@link Long#MAX_VALUE} if one hasn't been assigned
      */
