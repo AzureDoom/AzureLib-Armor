065b6e442fc5febe9a1891499682f0536985de4f
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-09-23 01:12:07 -0400

    Use root locale when converting strings - PlanetTeamSpeak

--- common/src/main/java/mod/azure/azurelibarmor/core/molang/MolangParser.java
+++ common/src/main/java/mod/azure/azurelibarmor/core/molang/MolangParser.java
@@ -1,6 +1,7 @@
 package mod.azure.azurelibarmor.core.molang;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.function.DoubleSupplier;
 
@@ -193,7 +194,7 @@ public class MolangParser extends MathBuilder {
 	public static MolangValue parseExpression(String expression) throws MolangException {
 		MolangCompoundValue result = null;
 
-		for (String split : expression.toLowerCase().trim().split(";")) {
+		for (String split : expression.toLowerCase(Locale.ROOT).trim().split(";")) {
 			String trimmed = split.trim();
 
 			if (!trimmed.isEmpty()) {
