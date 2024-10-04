0763cc0f541d4392405e1936cae6267798fa55d8
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-10-04 10:11:14 -0400

    Add GeoItemRender#useNewOffset() which will remove the y offset on item displays, due to change in BlockBench 4.11 item displays

--- common/src/main/java/mod/azure/azurelibarmor/renderer/GeoItemRenderer.java
+++ common/src/main/java/mod/azure/azurelibarmor/renderer/GeoItemRenderer.java
@@ -155,7 +155,7 @@ public class GeoItemRenderer<T extends Item & GeoAnimatable> extends BlockEntity
 		scaleModelForRender(this.scaleWidth, this.scaleHeight, poseStack, animatable, model, isReRender, partialTick, packedLight, packedOverlay);
 
 		if (!isReRender)
-			poseStack.translate(0.5f, 0.51f, 0.5f);
+			poseStack.translate(0.5f, this.useNewOffset() ? 0.0f :0.51f, 0.5f);
 	}
 
 	@Override
@@ -245,6 +245,17 @@ public class GeoItemRenderer<T extends Item & GeoAnimatable> extends BlockEntity
 	@Override
 	public void updateAnimatedTextureFrame(T animatable) {
 		AnimatableTexture.setAndUpdate(getTextureLocation(animatable), Item.getId(animatable) + (int) animatable.getTick(animatable));
+	}
+
+	/**
+	 * Determines whether to apply the y offset for a model due to the change in BlockBench 4.11.
+	 *
+	 * @return {@code false} by default, meaning the Y-offset will be {@code 0.51f}. Override this
+	 * method or change the return value to {@code true} to use the new Y-offset of {@code 0.0f}
+	 * for anything created in 4.11+ of Blockbench.
+	 */
+	public boolean useNewOffset() {
+		return false;
 	}
 
 	/**
