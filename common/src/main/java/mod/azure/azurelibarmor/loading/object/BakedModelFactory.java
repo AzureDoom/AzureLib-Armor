3a056b3fd2098529df990eaea958aba8b035482c
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-12-16 11:29:27 -0500

    Port UV Face Rotation support

--- common/src/main/java/mod/azure/azurelibarmor/loading/object/BakedModelFactory.java
+++ common/src/main/java/mod/azure/azurelibarmor/loading/object/BakedModelFactory.java
@@ -85,7 +85,7 @@ public interface BakedModelFactory {
 			if (faceUV == null)
 				return null;
 
-			return GeoQuad.build(vertices.verticesForQuad(direction, false, mirror || cube.mirror() == Boolean.TRUE), faceUV.uv(), faceUV.uvSize(),
+			return GeoQuad.build(vertices.verticesForQuad(direction, false, mirror || cube.mirror() == Boolean.TRUE), faceUV.uv(), faceUV.uvSize(), faceUV.uvRotation(),
 					textureWidth, textureHeight, mirror, direction);
 		}
 
@@ -119,7 +119,7 @@ public interface BakedModelFactory {
 			};
 		};
 
-		return GeoQuad.build(vertices.verticesForQuad(direction, true, mirror || cube.mirror() == Boolean.TRUE), uvData[0], uvData[1], textureWidth, textureHeight, mirror, direction);
+		return GeoQuad.build(vertices.verticesForQuad(direction, true, mirror || cube.mirror() == Boolean.TRUE), uvData[0], uvData[1], FaceUV.Rotation.NONE, textureWidth, textureHeight, mirror, direction);
 	}
 
 	static BakedModelFactory getForNamespace(String namespace) {
