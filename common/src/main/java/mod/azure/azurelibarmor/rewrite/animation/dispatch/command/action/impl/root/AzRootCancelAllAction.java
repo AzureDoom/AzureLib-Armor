610120935ce83c2303973ddf55ab45f0d4237739
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-01-19 15:44:09 -0500

    Fixes a packet issue with calling cancelAll on an AzCommand from the server.

--- common/src/main/java/mod/azure/azurelibarmor/rewrite/animation/dispatch/command/action/impl/root/AzRootCancelAllAction.java
+++ common/src/main/java/mod/azure/azurelibarmor/rewrite/animation/dispatch/command/action/impl/root/AzRootCancelAllAction.java
@@ -17,11 +17,13 @@ import net.minecraft.resources.ResourceLocation;
  */
 public class AzRootCancelAllAction implements AzAction {
 
-    public static final StreamCodec<FriendlyByteBuf, AzRootCancelAllAction> CODEC = StreamCodec.unit(
-        new AzRootCancelAllAction()
-    );
+    public static final AzRootCancelAllAction INSTANCE = new AzRootCancelAllAction();
+
+    public static final StreamCodec<FriendlyByteBuf, AzRootCancelAllAction> CODEC = StreamCodec.unit(INSTANCE);
 
     public static final ResourceLocation RESOURCE_LOCATION = AzureLib.modResource("root/cancel_all");
+
+    private AzRootCancelAllAction() {}
 
     @Override
     public void handle(AzDispatchSide originSide, AzAnimator<?> animator) {
