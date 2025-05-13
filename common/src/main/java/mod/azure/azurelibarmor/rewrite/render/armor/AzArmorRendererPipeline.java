39ecdf47b8f3a581a522013102770a0cbfb57891
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-05-13 22:24:46 +0200

    Avoid hardcoded white

--- common/src/main/java/mod/azure/azurelibarmor/rewrite/render/armor/AzArmorRendererPipeline.java
+++ common/src/main/java/mod/azure/azurelibarmor/rewrite/render/armor/AzArmorRendererPipeline.java
@@ -73,7 +73,7 @@ public class AzArmorRendererPipeline extends AzRendererPipeline<ItemStack> {
         boneContext.applyBoneVisibilityBySlot(currentSlot);
         if (ShoulderSurfingCompat.isLoaded() && ShoulderSurfingCompat.getAlpha() < 1) {
             int alpha = (int)(ShoulderSurfingCompat.getAlpha() * 0xFF) << 24;
-            int color = 0xFFFFFF | alpha;
+            int color = (armorContext.renderColor() & 0xFFFFFF) | alpha;
             armorContext.setRenderColor(color);
             armorContext.setTranslucent(true);
         }
