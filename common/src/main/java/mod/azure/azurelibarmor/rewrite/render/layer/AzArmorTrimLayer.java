4145f51dc65087cd6d9eb626f6e5fe78414f456f
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-10-08 10:27:26 -0400

    Cleanup/Add Java Docs

--- common/src/main/java/mod/azure/azurelibarmor/rewrite/render/layer/AzArmorTrimLayer.java
+++ common/src/main/java/mod/azure/azurelibarmor/rewrite/render/layer/AzArmorTrimLayer.java
@@ -1,20 +1,31 @@
 package mod.azure.azurelibarmor.rewrite.render.layer;
 
-import mod.azure.azurelibarmor.rewrite.model.AzBone;
-import mod.azure.azurelibarmor.rewrite.render.AzRendererPipelineContext;
-import mod.azure.azurelibarmor.rewrite.render.armor.AzArmorRendererPipelineContext;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.Sheets;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.armortrim.ArmorTrim;
-import com.mojang.blaze3d.vertex.VertexConsumer;
+
 import java.util.function.Function;
 
-public class AzArmorTrimLayer<T> implements AzRenderLayer<T> {
+import mod.azure.azurelibarmor.rewrite.model.AzBone;
+import mod.azure.azurelibarmor.rewrite.render.AzRendererPipelineContext;
+import mod.azure.azurelibarmor.rewrite.render.armor.AzArmorRendererPipelineContext;
+
+/**
+ * Represents a render layer for applying armor trim textures to an item stack during the rendering process. This layer
+ * enables dynamic customization of armor visuals by applying trim patterns and materials based on the item's metadata
+ * and associated {@link ArmorTrim}.
+ *
+ * @author ZsoltMolnarrr
+ */
+public class AzArmorTrimLayer implements AzRenderLayer<ItemStack> {
+
     public final ResourceLocation textureBaseLocation;
+
     public final Function<ArmorTrim, ResourceLocation> texturePermutations;
 
     public AzArmorTrimLayer(ResourceLocation baseTexture) {
@@ -22,35 +33,47 @@ public class AzArmorTrimLayer<T> implements AzRenderLayer<T> {
     }
 
     public AzArmorTrimLayer(ResourceLocation baseTexture, boolean supportPatterns) {
-        this(baseTexture, supportPatterns
-                ? (trim) -> {
+        this(
+            baseTexture,
+            supportPatterns
+                ? trim -> {
                     var pattern = trim.pattern().value();
                     var material = trim.material().value();
                     var patternName = pattern.assetId().getPath();
-                    return ResourceLocation.fromNamespaceAndPath(baseTexture.getNamespace(), baseTexture.getPath() + "_" + patternName + "_" + material.assetName());
+                    return ResourceLocation.fromNamespaceAndPath(
+                        baseTexture.getNamespace(),
+                        baseTexture.getPath() + "_" + patternName + "_" + material.assetName()
+                    );
                 }
-                : (trim) -> {
+                : trim -> {
                     var material = trim.material().value();
-                    return ResourceLocation.fromNamespaceAndPath(baseTexture.getNamespace(), baseTexture.getPath() + "_" + material.assetName());
+                    return ResourceLocation.fromNamespaceAndPath(
+                        baseTexture.getNamespace(),
+                        baseTexture.getPath() + "_" + material.assetName()
+                    );
                 }
         );
     }
 
-    public AzArmorTrimLayer(ResourceLocation baseTexture, Function<ArmorTrim, ResourceLocation> textureLocationPermutations) {
+    public AzArmorTrimLayer(
+        ResourceLocation baseTexture,
+        Function<ArmorTrim, ResourceLocation> textureLocationPermutations
+    ) {
         this.textureBaseLocation = baseTexture;
         this.texturePermutations = textureLocationPermutations;
     }
 
-    public void preRender(AzRendererPipelineContext<T> context) {
-    }
+    @Override
+    public void preRender(AzRendererPipelineContext<ItemStack> context) {}
 
-    public void render(AzRendererPipelineContext<T> context) {
+    @Override
+    public void render(AzRendererPipelineContext<ItemStack> context) {
         var armorPipelineContext = (AzArmorRendererPipelineContext) context;
         var itemstack = armorPipelineContext.currentStack();
         if (itemstack == null) {
             return;
         }
-        ArmorTrim armorTrim = itemstack.get(DataComponents.TRIM);
+        var armorTrim = itemstack.get(DataComponents.TRIM);
         if (armorTrim == null) {
             return;
         }
@@ -61,11 +84,11 @@ public class AzArmorTrimLayer<T> implements AzRenderLayer<T> {
         var armorTrimsAtlas = bakery.getAtlas(Sheets.ARMOR_TRIMS_SHEET); // Any way to get this from context?
 
         var renderPipeline = context.rendererPipeline();
-        ResourceLocation trimLocation = texturePermutations.apply(armorTrim);
+        var trimLocation = texturePermutations.apply(armorTrim);
 
-        TextureAtlasSprite sprite = armorTrimsAtlas.getSprite(trimLocation);
+        var sprite = armorTrimsAtlas.getSprite(trimLocation);
         var renderType = Sheets.armorTrimsSheet(pattern.decal());
-        VertexConsumer vertexConsumer = sprite.wrap(context.multiBufferSource().getBuffer(renderType));
+        var vertexConsumer = sprite.wrap(context.multiBufferSource().getBuffer(renderType));
 
         if (context.renderType() != null) {
             RenderType prevRenderType = context.renderType();
@@ -79,6 +102,5 @@ public class AzArmorTrimLayer<T> implements AzRenderLayer<T> {
     }
 
     @Override
-    public void renderForBone(AzRendererPipelineContext<T> azRendererPipelineContext, AzBone azBone) {
-    }
+    public void renderForBone(AzRendererPipelineContext<ItemStack> azRendererPipelineContext, AzBone azBone) {}
 }
