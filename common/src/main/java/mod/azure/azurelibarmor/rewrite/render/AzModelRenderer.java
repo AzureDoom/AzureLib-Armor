28679748188fe75f86f2097e8642ed95dc0720be
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-10-08 08:58:48 -0400

    Changes to AzAnimator to add support for stopping the animation timer

--- common/src/main/java/mod/azure/azurelibarmor/rewrite/render/AzModelRenderer.java
+++ common/src/main/java/mod/azure/azurelibarmor/rewrite/render/AzModelRenderer.java
@@ -14,6 +14,7 @@ import mod.azure.azurelibarmor.common.internal.client.util.RenderUtils;
 import mod.azure.azurelibarmor.common.internal.common.cache.object.GeoCube;
 import mod.azure.azurelibarmor.common.internal.common.cache.object.GeoQuad;
 import mod.azure.azurelibarmor.common.internal.common.cache.object.GeoVertex;
+import mod.azure.azurelibarmor.rewrite.animation.AzAnimator;
 import mod.azure.azurelibarmor.rewrite.model.AzBone;
 
 /**
@@ -231,5 +232,9 @@ public class AzModelRenderer<T> {
     @Nullable
     public ResourceLocation getTextureOverrideForBone(AzBone bone, T animatable, float partialTick) {
         return null;
+    }
+
+    public void handleAnimation(AzAnimator<T> animator, T animatable, float partialTick) {
+        animator.animate(animatable, partialTick);
     }
 }
