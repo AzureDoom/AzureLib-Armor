20cdde0bbb6f526635bebc0b19659b109f4e4c5b
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2025-11-28 16:39:23 -0500

    Fixed an issue where items couldn't use client side AzCommands.

--- common/src/main/java/mod/azure/azurelibarmor/common/animation/dispatch/command/AzCommand.java
+++ common/src/main/java/mod/azure/azurelibarmor/common/animation/dispatch/command/AzCommand.java
@@ -166,7 +166,7 @@ public record AzCommand(List<AzAction> actions) {
      */
     public void sendForItem(Entity entity, ItemStack itemStack) {
         if (entity.level().isClientSide()) {
-            dispatchFromClient(entity);
+            dispatchFromClient(itemStack);
         } else {
             var uuid = itemStack.get(AzureLib.AZ_ID.get());
 
